d125bd9222cd042734e7e95320d91013e6f82213
YahyaAr <yahya.arba@ig2i.fr>
2016-06-14 17:03:55 +0200

    Partie 5 + 6 OK
    Niveau 1 + 2

---
+++ New Unity Project/Assets/NextLevel.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var levelName : String = "Entrez le nom du niveau ";
+
+
+function OnTriggerEnter2D (other : Collider2D) {
+
+	if(other.tag == "Player"){
+		Application.LoadLevel(levelName);
+	}
+}
---
+++ New Unity Project/Assets/fallingPlateform.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var platform : GameObject;
+
+var sound : AudioClip;
+private var hasPlayed= false;
+
+
+function OnTriggerEnter2D (other : Collider2D) {
+
+	if(other.tag == "Player"){
+		Debug.Log("Detected Collision");
+		if(hasPlayed ==false){
+			GetComponent.<AudioSource>().PlayOneShot(sound);
+			hasPlayed=true;
+		}
+		platform.GetComponent.<Rigidbody2D>().isKinematic = false;
+			
+	}
+
+}
